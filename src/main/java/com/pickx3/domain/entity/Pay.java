c32a71f9f1fb6d377b1ecb1d2f6135e029f438b3
ted <sshted5595@gmail.com>
2023-01-20 14:52:04 +0900

    git1123

---
+++ src/main/java/com/pickx3/domain/entity/Pay.java
@@ -0,0 +1,13 @@
+package com.pickx3.domain.entity;
+
+import java.util.Date;
+
+public class Pay{
+    private Long payNum;
+    private Long payerNum;
+    private Long workNum;
+    private Date payDate;
+    private int payPrice;
+    private String payMethod;
+    private int payCount;
+}
---
+++ src/main/java/com/pickx3/domain/entity/Work.java
@@ -0,0 +1,9 @@
+package com.pickx3.domain.entity;
+
+public class Work {
+    private Long workNum;
+    private Long workerNum;
+    private String workName;
+    private int workPirce;
+    private String workDesc;
+}
