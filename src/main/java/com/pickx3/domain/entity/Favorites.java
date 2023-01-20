2addbb1a187e38fc1c555b3d2bd6e1f70128b737
KIM-DO-HEE <kjkj014209@gmail.com>
2023-01-20 14:44:41 +0900

    Post, Comment, Favorites, PostImg 엔티티 추가

---
+++ src/main/java/com/pickx3/domain/entity/Favorites.java
@@ -0,0 +1,7 @@
+package com.pickx3.domain.entity;
+
+public class Favorites {
+    private Long favoritesNum;
+    private Long userNum;
+    private Long portfolioNum;
+}
---
+++ src/main/java/com/pickx3/domain/entity/Post.java
@@ -0,0 +1,11 @@
+package com.pickx3.domain.entity;
+
+import java.time.LocalDateTime;
+
+public class Post {
+    private Long postNum;
+    private Long userNum;
+    private String postTitle;
+    private String postContent;
+    private LocalDateTime postDate;
+}
